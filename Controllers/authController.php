57d52c19daa6557d9835082b9f6dd9b73035d502
Paschal Timoth <droidgrim@gmail.com>
2025-06-05 17:15:16 +0300

    resolved Cors issues

--- Controllers/authController.php
+++ Controllers/authController.php
@@ -1,4 +1,15 @@
 <?php
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require '../vendor/autoload.php';
 require '../Config/config.php';
 
